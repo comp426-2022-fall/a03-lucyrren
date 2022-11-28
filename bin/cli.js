fc8957b26c507c4de2b238f94aa7f81fc132192d
Lucy <lucyren@email.unc.edu>
2022-11-27 22:15:20 -0500

    attempt 1

---
+++ bin/cli.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import minimist from 'minimist';
+import { roll } from "../lib/roll.js"
+
+let args = minimist(process.argv.slice(2));
+
+let sides = 6;
+let dice = 2;
+let rolls = 1;
+if(args.sides && args.sides > 0){
+    sides = args.sides;
+}
+if(args.dice && args.dice > 0){
+    dice = args.dice;
+}
+if(args.rolls && args.rolls > 0){
+    rolls = args.roll;
+}
+
+console.log(JSON.stringify(roll(sides, dice, rolls)));
